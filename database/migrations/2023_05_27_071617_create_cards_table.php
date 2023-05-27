ca2a70e9162a3cdb38317fe8583c5851bfc7a662
Chee Chong Yuong <jerrychee1994@gmail.com>
2023-05-27 15:36:53 +0800

    feat: db initialize

---
+++ database/migrations/2023_05_27_071617_create_cards_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('currency');
+            $table->string('balance');
+            $table->string('type');
+            $table->string('issuer_id_number');
+            $table->string('card_holder_name');
+            $table->string('ccv', 3);
+            $table->string('status');
+            $table->date('expiry_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+};
---
+++ database/migrations/2023_05_27_071705_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('card_id');
+            $table->unsignedBigInteger('merchant_user_id');
+            $table->string('type');
+            $table->string('currency', 3);
+            $table->float('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
